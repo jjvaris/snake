e7b5dfec34d4c99d8de8f3599db90a52c85f30d5
Jouni Varis <jjvaris@gmail.com>
2019-05-29 19:32:50 +0300

    Working two player snake before refactoring

--- snake-io-frontend/src/App.js
+++ snake-io-frontend/src/App.js
@@ -2,39 +2,55 @@ import React, { useRef, useEffect } from 'react';
 import './App.css';
 import Snake from './snake';
 
-const state = {
-  rows: 60,
-  columns: 80,
-  snake: [{ x: 5, y: 10 }, { x: 6, y: 10 }],
-  apple: { x: 50, y: 40 }
-};
-
-const x = c => Math.round((c * canvasWidth) / state.columns);
-const y = r => Math.round((r * canvasHeight) / state.rows);
+const x = c => Math.round((c * canvasWidth) / 80);
+const y = r => Math.round((r * canvasHeight) / 60);
 
 const canvasWidth = 800;
 const canvasHeight = 600;
 
 function App() {
   const canvas = useRef(null);
-  const state = Snake.initialState();
+
   let moves = [];
   useEffect(() => {
+    console.log('useEffect');
+    const state = Snake.initialState();
     draw(canvas.current, state);
     requestAnimationFrame(loop(state, 0));
     document.addEventListener('keydown', e => {
+      console.log(e);
       switch (e.key) {
         case 'ArrowUp':
-          moves.push({ id: '1', direction: 'NORTH' });
+          Snake.enqueueAction({ id: 's1', direction: 'NORTH' });
+          //moves.push({ id: '1', direction: 'NORTH' });
           break;
         case 'ArrowDown':
-          moves.push({ id: '1', direction: 'SOUTH' });
+          Snake.enqueueAction({ id: 's1', direction: 'SOUTH' });
+          // moves.push({ id: '1', direction: 'SOUTH' });
           break;
         case 'ArrowLeft':
-          moves.push({ id: '1', direction: 'WEST' });
+          Snake.enqueueAction({ id: 's1', direction: 'WEST' });
+          // moves.push({ id: '1', direction: 'WEST' });
           break;
         case 'ArrowRight':
-          moves.push({ id: '1', direction: 'EAST' });
+          Snake.enqueueAction({ id: 's1', direction: 'EAST' });
+          // moves.push({ id: '1', direction: 'EAST' });
+          break;
+        case 'w':
+          Snake.enqueueAction({ id: 's2', direction: 'NORTH' });
+          //moves.push({ id: '1', direction: 'NORTH' });
+          break;
+        case 's':
+          Snake.enqueueAction({ id: 's2', direction: 'SOUTH' });
+          // moves.push({ id: '1', direction: 'SOUTH' });
+          break;
+        case 'a':
+          Snake.enqueueAction({ id: 's2', direction: 'WEST' });
+          // moves.push({ id: '1', direction: 'WEST' });
+          break;
+        case 'd':
+          Snake.enqueueAction({ id: 's2', direction: 'EAST' });
+          // moves.push({ id: '1', direction: 'EAST' });
           break;
       }
     });
@@ -42,10 +58,7 @@ function App() {
 
   const loop = (state, t1) => t2 => {
     if (t2 - t1 > 35) {
-      const nextState = Snake.nextState(
-        state.snakes['1'].body.length === 0 ? Snake.initialState() : state,
-        moves.shift() || { id: '1', direction: state.snakes['1'].direction }
-      );
+      const nextState = Snake.next();
       draw(canvas.current, nextState);
       requestAnimationFrame(loop(nextState, t2));
     } else {
@@ -67,18 +80,26 @@ function draw(canvas, state) {
   ctx.fillStyle = '#232323';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  // draw snake
+  // draw snake1
   ctx.fillStyle = 'pink';
-  state.snakes['1'].body.map(p => ctx.fillRect(x(p.x), y(p.y), x(1), y(1)));
+  state.snakes['s1'].body.map(p => ctx.fillRect(x(p.x), y(p.y), x(1), y(1)));
+
+  // draw snake
+  ctx.fillStyle = 'green';
+  state.snakes['s2'].body.map(p => ctx.fillRect(x(p.x), y(p.y), x(1), y(1)));
 
   // draw apple
   ctx.fillStyle = 'red';
   ctx.fillRect(x(state.apple.x), y(state.apple.y), x(1), y(1));
 
   // draw endgame
-  if (state.snakes['1'].body.length === 0) {
+  if (
+    state.snakes['s1'].body.length === 0 ||
+    state.snakes['s2'].body.length === 0
+  ) {
     ctx.fillStyle = 'red';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+    Snake.initialState();
   }
   // console.log('dwaring');
   // state.snake.map(p => {
--- snake-io-frontend/src/snake.js
+++ snake-io-frontend/src/snake.js
@@ -5,6 +5,14 @@ const SOUTH = 'SOUTH';
 
 const mod = (n, m) => ((n % m) + m) % m;
 const rnd = max => Math.floor(Math.random() * max + 1);
+const removeFirst = array => array.slice(1);
+const keysToArray = o => [
+  ...Object.entries(o).map(([k, v]) => {
+    return { ...v, id: k };
+  })
+];
+const getSnakeHead = snake => snake.body[snake.body.length - 1];
+const isSamePosition = (pos1, pos2) => pos1.x === pos2.x && pos1.y === pos2.y;
 
 const directions = {
   EAST: { x: 1, y: 0 },
@@ -13,45 +21,211 @@ const directions = {
   SOUTH: { x: 0, y: 1 }
 };
 
+let state = {};
+
 const snake = {};
 
 snake.initialState = () => {
-  return {
+  console.log('initial state called');
+  state = {
+    gameOver: false,
     rows: 60,
     columns: 80,
     snakes: {
-      '1': {
-        body: [{ x: 5, y: 10 }, { x: 6, y: 10 }],
-        direction: EAST
+      s1: {
+        body: [
+          { x: 6, y: 10 },
+          { x: 7, y: 10 },
+          { x: 8, y: 10 },
+          { x: 9, y: 10 },
+          { x: 10, y: 10 },
+          { x: 11, y: 10 },
+          { x: 12, y: 10 },
+          { x: 13, y: 10 },
+          { x: 14, y: 10 },
+          { x: 15, y: 10 }
+        ],
+        direction: EAST,
+        moves: [],
+        points: 0
+      },
+      s2: {
+        body: [
+          { x: 6, y: 40 },
+          { x: 7, y: 40 },
+          { x: 8, y: 40 },
+          { x: 9, y: 40 },
+          { x: 10, y: 40 },
+          { x: 11, y: 40 },
+          { x: 12, y: 40 },
+          { x: 13, y: 40 },
+          { x: 14, y: 40 },
+          { x: 15, y: 40 }
+        ],
+        direction: EAST,
+        moves: [],
+        points: 0
       }
     },
-    apple: { x: 50, y: 40 }
+    apple: { x: rnd(80 - 1), y: rnd(60 - 1) }
+  };
+  return state;
+};
+
+const newApple = () => {
+  return { x: rnd(state.columns - 1), y: rnd(state.rows - 1) };
+};
+
+snake.enqueueAction = action => {
+  const snake = state.snakes[action.id];
+  state = {
+    ...state,
+    snakes: {
+      ...state.snakes,
+      [action.id]: {
+        ...snake,
+        moves: [...snake.moves, action.direction]
+      }
+    }
   };
 };
 
-snake.nextState = (state, action) => {
-  return isValid(state, action)
-    ? move(state, action.id, action.direction)
-    : move(state, action.id, state.snakes[action.id].direction);
+snake.next = () => {
+  // moveSnakes, eat etc.
+  // check collisions
+  const snakesWithNewPositions = moveSnakes(state);
+  const aliveSnakes = removeCollidedSnakes(keysToArray(snakesWithNewPositions));
+  console.log(aliveSnakes);
+  const apple = isAppleEated(state.apple, aliveSnakes)
+    ? newApple()
+    : state.apple;
+
+  //console.log(snakesWithNewPositions);
+  //console.log(aliveSnakes);
+  state = {
+    ...state,
+    snakes: aliveSnakes.reduce((snakes, snake) => {
+      snakes[snake.id] = snake;
+      return snakes;
+    }, {}),
+    apple: apple
+  };
+  //console.log(state);
+  return state;
+  //state = collision(state) ? stateAfterCollision(state) : curState;
 };
 
-const isValid = (state, action) => {
-  const direction = state.snakes[action.id].direction;
-  switch (action.direction) {
+const moveSnakes = state => {
+  const snakes = Object.entries(state.snakes)
+    .map(([id, snake]) => {
+      const head = snake.body[snake.body.length - 1];
+      const direction =
+        snake.moves.length !== 0
+          ? isValid(snake.moves[0], snake.direction)
+            ? snake.moves[0]
+            : snake.direction
+          : snake.direction;
+      const nextPosition = {
+        x: mod(head.x + directions[direction].x, state.columns),
+        y: mod(head.y + directions[direction].y, state.rows)
+      };
+      const willEat = () =>
+        state.apple.x === nextPosition.x && state.apple.y === nextPosition.y;
+
+      const body = willEat() ? snake.body : removeFirst(snake.body);
+      return [
+        id,
+        {
+          ...snake,
+          body: [...body, nextPosition],
+          moves: snake.moves ? [...removeFirst(snake.moves)] : [],
+          direction: direction
+        }
+      ];
+    })
+    .reduce((snakes, [id, snake]) => {
+      snakes[id] = snake;
+      return snakes;
+    }, {});
+  return snakes;
+};
+
+const removeCollidedSnakes = snakes => {
+  const isOwnHead = (snake1, snake2, body) => {
+    const head1 = snake1.body[snake1.body.length - 1];
+    const head2 = snake2.body[snake2.body.length - 1];
+    return head2.x === body.y && head2.y === body.y && snake1.id === snake2.id;
+    // if(head2.x === body.y && head2.y === body.y && snake1.id === snake2.id)
+    //  return true
+    // return (
+    //   snake1.id === snake2.id && head1.x === head2.x && head1.y === body.y
+    // );
+  };
+
+  const willCollide = snake => {
+    const head = snake.body[snake.body.length - 1];
+
+    const ownCollision = () => {
+      const body = snake.body.slice(0, snake.body.length - 1);
+      return body.some(pos => pos.x === head.x && pos.y === head.y);
+    };
+
+    return ownCollision()
+      ? true
+      : snakes.some(s => {
+          return s.body.some(
+            b => s.id !== snake.id && b.x === head.x && b.y === head.y
+            // b.y === head.y &&
+            // !(
+            //   s.id === snake.id &&
+            //   s.body[s.body.length - 1].x === b.x &&
+            //   s.body[s.body.length - 1].y === b.y
+            // )
+          );
+        });
+  };
+
+  return snakes.map(snake => {
+    if (willCollide(snake)) {
+      console.log('COLLISION');
+      return {
+        ...snake,
+        body: []
+      };
+    }
+    console.log('NO COLLISION');
+    return snake;
+  });
+};
+
+const isAppleEated = (apple, snakes) =>
+  snakes.some(
+    snake => snake.body.length > 0 && isSamePosition(apple, getSnakeHead(snake))
+  );
+
+snake.nextState = action => {
+  state = isValid(action)
+    ? move(action.id, action.direction)
+    : move(action.id, state.snakes[action.id].direction);
+  return state;
+};
+
+const isValid = (action, currentDirection) => {
+  switch (action) {
     case EAST:
-      return direction !== WEST;
+      return currentDirection !== WEST;
     case WEST:
-      return direction !== EAST;
+      return currentDirection !== EAST;
     case NORTH:
-      return direction !== SOUTH;
+      return currentDirection !== SOUTH;
     case SOUTH:
-      return direction !== NORTH;
+      return currentDirection !== NORTH;
     default:
       return false;
   }
 };
 
-const move = (state, id, direction) => {
+const move = (id, direction) => {
   const snake = state.snakes[id];
   const head = snake.body[snake.body.length - 1];
   const nextPosition = {
